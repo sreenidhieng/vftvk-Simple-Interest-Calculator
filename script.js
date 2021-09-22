e3138236978fc2cbd672832764da8e1edba09563
sreenidhieng <90587052+sreenidhieng@users.noreply.github.com>
2021-09-22 20:54:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 function compute()
 {
     p = document.getElementById("principal").value;
+    n = document.getElementById("number").value;
+    r = document.getElementById("rate").value;
+    result = document.getElementById("result");
+    result.innerHTML= "The interest is" + (p*n*r/100);
     
 }
         
